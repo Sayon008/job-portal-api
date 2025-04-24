0c3422ade6ac9f7527ba84337672b988b9c4ceec
Sayon Batabyal <batabyalsayon@gmail.com>
2025-04-25 02:53:39 +0530

    Initial Spring Boot setup

---
+++ src/main/java/org/vibe/jobportal/controller/HelloController.java
@@ -0,0 +1,13 @@
+package org.vibe.jobportal.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "Hello, Our Spring Boot API is working fine";
+    }
+}
