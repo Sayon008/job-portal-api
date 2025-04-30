a4f12e7733ac80bcc6201f1d5526312e5be31675
Sayon Batabyal <batabyalsayon@gmail.com>
2025-05-01 02:27:57 +0530

    User Repository

---
+++ src/main/java/org/vibe/jobportal/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package org.vibe.jobportal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.vibe.jobportal.model.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    //    Find User By Name
+    Optional<User> findByUsername(String username);
+}
+
