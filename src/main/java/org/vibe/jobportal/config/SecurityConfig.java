ff76812b17409bde76016c3602917979561d2572
Sayon Batabyal <batabyalsayon@gmail.com>
2025-05-04 01:16:13 +0530

    Custom Authentication Provider class which handles custom authentication

--- src/main/java/org/vibe/jobportal/config/SecurityConfig.java
+++ src/main/java/org/vibe/jobportal/config/SecurityConfig.java
@@ -1,13 +1,21 @@
 package org.vibe.jobportal.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.vibe.jobportal.security.CustomAuthenticationProvider;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private CustomAuthenticationProvider customAuthenticationProvider;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -20,5 +28,10 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration){
+        return new ProviderManager(customAuthenticationProvider);
     }
 }
